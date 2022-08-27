bd69d8b6d659a6e511b71c38000bef7711ef47a8
Ethan Sprinkle <els2357@mavs.uta.edu>
2022-08-26 20:18:49 -0500

    P01 bonus

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Ethan Sprinkle!");
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println("Hello, " + line + "!");
     }
 }
