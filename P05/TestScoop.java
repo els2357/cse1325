f3d9a5eeb06f244178ad08fa881e1a52b6960420
Ethan Sprinkle <els2357@mavs.uta.edu>
2022-10-02 18:13:08 -0500

    Interactive builder with IceCreamFlavor and MixInFlavor working. May continue to test using regression.

--- P05/TestScoop.java
+++ P05/TestScoop.java
@@ -5,24 +5,73 @@ public class TestScoop{
 		System.out.print(
 		"=================\n\r" +
 		"MICE TESTER v0.01\n\r" +
-		"=================\n\r"
+		"=================\n\n\r"
 		);
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Create new (m)ixin, (i)ce cream flavor, or (q)uit? ");
-        char input = scanner.next().charAt(0);
-        while (input != 'i' && input != 'm' && input != 'q'){
-        	System.out.print("You didn't select properly! Try Again!\n");
+		char input;
+        while (true){
+        	System.out.print("Create new (m)ixin, (i)ce cream flavor, (s)coop, or (q)uit? ");
         	input = scanner.next().charAt(0);
-        }
-        if (input=='i'){
-        	System.out.print("Creating new Ice Cream Flavor!\n");
-        }
-        else if (input=='m'){
-        	System.out.print("Creating new MixIn Flavor!\n");
-        }
-        else if (input=='q'){
-        	System.out.print("Goodbye!\n");
-        }
+
+        	if (input=='i'){
+        		System.out.print("Creating new Ice Cream Flavor!\n\n");        	
+  	     		String buffer = scanner.nextLine();
+  	     		System.out.print("Name? ");
+        		String name = scanner.nextLine();
+        		System.out.print("Description? ");
+        		String description = scanner.nextLine();
+        		System.out.print("Price? ");
+        		int price = scanner.nextInt();
+        		System.out.print("Cost? ");
+        		int cost = scanner.nextInt();
+        		new IceCreamFlavor(name, description, price, cost);
+        		
+        		System.out.print(
+				"\n" + 
+				"=================\n\r" +
+				"MICE TESTER v0.01\n\r" +
+				"=================\n\n\r"
+				);       
+        	}
+        	
+        	else if (input=='m'){
+        		System.out.print("Creating new MixIn Flavor!\n\n");
+        		
+        		String buffer = scanner.nextLine();       	
+  	     		System.out.print("Name? ");
+        		String name = scanner.nextLine();
+        		System.out.print("Description? ");
+        		String description = scanner.nextLine();
+        		System.out.print("Price? ");
+        		int price = scanner.nextInt();
+        		System.out.print("Cost? ");
+        		int cost = scanner.nextInt();
+        		
+        		new MixInFlavor(name, description, price, cost);
+        		
+				System.out.print(
+				"\n" + 
+				"=================\n\r" +
+				"MICE TESTER v0.01\n\r" +
+				"=================\n\n\r"
+				);       
+        	}
+        	
+        	
+        	else if (input=='s'){
+        		System.out.print("Creating a Scoop of Ice cream!\n");
+        		//new Scoop(vanilla);
+        	}
+        	
+        	else if (input=='q'){
+        		System.out.print("Goodbye!\n");
+        		System.exit(1);
+        	}
+        	
+        	else {
+        		System.out.print("Incorrect Input. Try Again!\n");
+        	}
+		}
 	}
 }
