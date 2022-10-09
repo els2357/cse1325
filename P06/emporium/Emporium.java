eb4bcb0bce05bf0b5d6023fa7296336e31cc7561
Ethan Sprinkle <els2357@mavs.uta.edu>
2022-10-09 18:59:55 -0500

    Added Emporium class and basis for MainWin class.

--- P06/emporium/Emporium.java
+++ P06/emporium/Emporium.java
@@ -1,3 +1,42 @@
+package emporium;
+
+import java.util.ArrayList;
+import product.IceCreamFlavor;
+import product.MixInFlavor;
+import product.MixInAmount;
+import product.MixIn;
+import product.Scoop;
+
 public class Emporium{
-	private 
+	private ArrayList<MixInFlavor> mixInFlavors = new ArrayList <>();
+	private ArrayList<IceCreamFlavor> iceCreamFlavors = new ArrayList <>();
+	private ArrayList<Scoop> scoops = new ArrayList <>();
+	//public Object[] al = new Object[];
+	
+	public void addMixInFlavor(MixInFlavor flavor){
+		mixInFlavors.add(flavor);
+	}
+	
+	public void addIceCreamFlavor(IceCreamFlavor flavor){
+		iceCreamFlavors.add(flavor);
+	}
+	
+	public void addScoop(Scoop scoop){
+		scoops.add(scoop);
+	}
+	
+	public Object[] mixInFlavors(){
+		Object[] mixInFlavorsObject = mixInFlavors.toArray();
+		return mixInFlavorsObject;
+	}
+	
+	public Object[] iceCreamFlavors(){
+		Object[] iceCreamFlavorsObject = iceCreamFlavors.toArray();
+		return iceCreamFlavorsObject;
+	}
+	
+	public Object[] scoops(){
+		Object[] scoopsObject = scoops.toArray();		
+		return scoopsObject;
+	}
 }
--- P06/gui/MainWin.java
+++ P06/gui/MainWin.java
@@ -1,3 +1,37 @@
-public class MainWin{
+package gui;
+
+import emporium.Emporium;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+
+public class MainWin extends JFrame {// implements ActionListener {
+	private Emporium emporium;
+	private JLabel display;
 	
+    public void MainWin(){
+    }
+    
+    public void onQuitClick(){
+    }
+    
+    public void onCreateIceCreamFlavorClick(){
+    }
+    
+    public void onCreateMixInFlavorClick(){
+    }
+    
+    public void onCreateScoopClick(){
+    }
+    
+    public void onAboutClick(){
+    }
+    
+    private void view(Screen screen){
+    }
 }
